a4780a9d11effb2bc75d1c2aed96fce3a35789de
softniels-felipe-ferrandin <80527150+softniels-felipe-ferrandin@users.noreply.github.com>
2022-05-19 16:11:24 -0300

    refactor

--- purchases/src/server.ts
+++ purchases/src/server.ts
@@ -1,14 +1,14 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
 import dotenv from "dotenv"
-import bodyParser from "body-parser";
-import compression from "compression";
-import helmet from "helmet";
-import morgan from "morgan";
+import bodyParser from "body-parser"
+import compression from "compression"
+import helmet from "helmet"
+import morgan from "morgan"
 import cors from "cors"
-import CorsConfigurationUtilities from "@/framework/utilities/cors/CorsConfigurationUtilities";
-import {CustomerRouter} from "@/customer/CustomerRouter";
-import {ProductRouter} from "./product/ProductRouter";
-import {PurchaseRouter} from "@/purchase/PurchaseRouter";
+import CorsConfigurationUtilities from "@/framework/utilities/cors/CorsConfigurationUtilities"
+import { CustomerRouter } from "@/customer/CustomerRouter"
+import { ProductRouter } from "./product/ProductRouter"
+import { PurchaseRouter } from "@/purchase/PurchaseRouter"
 
 dotenv.config()
 
@@ -25,8 +25,8 @@ app.use("/customer", CustomerRouter)
 app.use("/product", ProductRouter)
 app.use("/purchase", PurchaseRouter)
 
-app.use((err: Error, req: Request, res: Response, next: Function) => {
-
+app.use((err: Error, req: Request, res: Response, next: () => void) => {
+    //
 })
 
 app.listen(process.env.APPLICATION_PORT, () => console.log(`Application is running on port ${process.env.APPLICATION_PORT}`))
