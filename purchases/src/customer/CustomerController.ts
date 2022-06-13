b57a49923a6146bfcda9ce2c5006a662cde0199d
Feliep <felipeferrandin@gmail.com>
2022-06-12 22:09:19 -0300

    refatorado

--- purchases/src/customer/CustomerController.ts
+++ purchases/src/customer/CustomerController.ts
@@ -1,6 +1,5 @@
-import {ICustomerAPI} from "@/customer/ICustomerAPI";
-import {Request, Response} from "express";
-import {DefaultErrorResponseDTO} from "@/framework/errors/DefaultErrorResponseDTO";
+import {ICustomerAPI} from "@/customer/ICustomerAPI"
+import {Request, Response} from "express"
 
 class CustomerController {
 
@@ -8,31 +7,14 @@ class CustomerController {
     }
 
     async createCustomer(aRequest: Request, aResponse: Response) {
-        try {
-            const lCustomerDTO = aRequest.body
-            await this.mICustomerAPI.createCustomer(lCustomerDTO)
-            aResponse.status(200).json({message: 'Cliente inserido com sucesso!'})
-        } catch (e) {
-            console.error(e)
-            aResponse.status(422).json(<DefaultErrorResponseDTO>{
-                error: e,
-                message: "Erro ao inserir cliente!",
-                code: 422
-            })
-        }
+        const lCustomerDTO = aRequest.body
+        await this.mICustomerAPI.createCustomer(lCustomerDTO)
+        aResponse.status(200).json({message: 'Cliente inserido com sucesso!'})
     }
 
     async getCompleteCustomer(aRequest: Request, aResponse: Response) {
-        try {
-            const lIdCustomer = Number(aRequest.params.id)
-            aResponse.status(200).json(this.mICustomerAPI.getCompleteCustomer(lIdCustomer))
-        } catch (e) {
-            aResponse.status(422).json(<DefaultErrorResponseDTO>{
-                error: e,
-                message: "Erro ao buscar cliente!",
-                code: 422
-            })
-        }
+        const lIdCustomer = Number(aRequest.params.id)
+        aResponse.status(200).json(this.mICustomerAPI.getCompleteCustomer(lIdCustomer))
     }
 }
 
