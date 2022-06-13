f15a852afb769dbd19902bda1eb4a78ee871071b
Feliep <felipeferrandin@gmail.com>
2022-06-12 22:11:21 -0300

    adicionado metodos para finalizar e cancelar a compra

--- purchases/src/purchase/PurchaseRouter.ts
+++ purchases/src/purchase/PurchaseRouter.ts
@@ -1,5 +1,5 @@
-import { Router } from "express"
-import { gPurchaseController } from "@/purchase/PurchaseModule"
+import {Router} from "express"
+import {gPurchaseController} from "@/purchase/PurchaseModule"
 
 const PurchaseRouter = Router()
 
@@ -11,16 +11,20 @@ PurchaseRouter.get("/:idPurchase", (req, res) => {
     return gPurchaseController.getPurchaseById(req, res)
 })
 
-PurchaseRouter.delete("/:idPurchase", (req, res) => {
-    return gPurchaseController.deletePurchaseById(req, res)
-})
-
 PurchaseRouter.put("/:idPurchase", (req, res) => {
     return gPurchaseController.insertProductInPurchase(req, res)
+})
+
+PurchaseRouter.put("/finalize/:idPurchase", (req, res) => {
+    return gPurchaseController.finalizePurchase(req, res)
+})
+
+PurchaseRouter.put("/cancel/:idPurchase", (req, res) => {
+    return gPurchaseController.cancelPurchase(req, res)
 })
 
 PurchaseRouter.delete("/:idPurchase/:idPurchaseDetail", (req, res) => {
     return gPurchaseController.deleteProductPurchaseById(req, res)
 })
 
-export { PurchaseRouter }
+export {PurchaseRouter}
